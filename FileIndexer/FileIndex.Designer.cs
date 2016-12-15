baaa3c1c39d60d8f33bb9bfd0b02bef2d92d5f56
Ivan Ilev <ivanilev95@gmail.com>
2016-12-15 17:18:51 +0200

    General layout commit.

--- FileIndexer/FileIndex.Designer.cs
+++ FileIndexer/FileIndex.Designer.cs
@@ -47,7 +47,17 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.cmsNodeClick = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.tsmOpenImage = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.indexerControllerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panelTopContainer.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
@@ -63,23 +73,33 @@
             // 
             // panelTopContainer
             // 
+            this.panelTopContainer.Controls.Add(this.textBox2);
+            this.panelTopContainer.Controls.Add(this.label5);
+            this.panelTopContainer.Controls.Add(this.dateTimePicker2);
+            this.panelTopContainer.Controls.Add(this.label4);
+            this.panelTopContainer.Controls.Add(this.dateTimePicker1);
+            this.panelTopContainer.Controls.Add(this.label3);
+            this.panelTopContainer.Controls.Add(this.label2);
+            this.panelTopContainer.Controls.Add(this.comboBox2);
+            this.panelTopContainer.Controls.Add(this.textBox1);
+            this.panelTopContainer.Controls.Add(this.comboBox1);
             this.panelTopContainer.Controls.Add(this.btnSearch);
             this.panelTopContainer.Controls.Add(this.btnSelectFolder);
             this.panelTopContainer.Controls.Add(this.tbSearch);
             this.panelTopContainer.Controls.Add(this.lblSearch);
             this.panelTopContainer.Controls.Add(this.label1);
-            this.panelTopContainer.Location = new System.Drawing.Point(12, 12);
+            this.panelTopContainer.Location = new System.Drawing.Point(12, 0);
             this.panelTopContainer.Name = "panelTopContainer";
-            this.panelTopContainer.Size = new System.Drawing.Size(984, 73);
+            this.panelTopContainer.Size = new System.Drawing.Size(984, 85);
             this.panelTopContainer.TabIndex = 0;
             // 
             // btnSearch
             // 
             this.btnSearch.Enabled = false;
-            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnSearch.Location = new System.Drawing.Point(821, 21);
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnSearch.Location = new System.Drawing.Point(529, 13);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(140, 37);
+            this.btnSearch.Size = new System.Drawing.Size(100, 25);
             this.btnSearch.TabIndex = 5;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -87,10 +107,10 @@
             // 
             // btnSelectFolder
             // 
-            this.btnSelectFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnSelectFolder.Location = new System.Drawing.Point(264, 19);
+            this.btnSelectFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnSelectFolder.Location = new System.Drawing.Point(136, 13);
             this.btnSelectFolder.Name = "btnSelectFolder";
-            this.btnSelectFolder.Size = new System.Drawing.Size(201, 37);
+            this.btnSelectFolder.Size = new System.Drawing.Size(100, 25);
             this.btnSelectFolder.TabIndex = 3;
             this.btnSelectFolder.Text = "Browse...";
             this.btnSelectFolder.UseVisualStyleBackColor = true;
@@ -99,29 +119,30 @@
             // tbSearch
             // 
             this.tbSearch.Enabled = false;
-            this.tbSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.tbSearch.Location = new System.Drawing.Point(628, 25);
+            this.tbSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbSearch.Location = new System.Drawing.Point(323, 14);
             this.tbSearch.Name = "tbSearch";
-            this.tbSearch.Size = new System.Drawing.Size(187, 29);
+            this.tbSearch.Size = new System.Drawing.Size(200, 23);
             this.tbSearch.TabIndex = 3;
+            this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
             // 
             // lblSearch
             // 
             this.lblSearch.AutoSize = true;
-            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblSearch.Location = new System.Drawing.Point(471, 12);
+            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSearch.Location = new System.Drawing.Point(242, 14);
             this.lblSearch.Name = "lblSearch";
-            this.lblSearch.Size = new System.Drawing.Size(151, 44);
+            this.lblSearch.Size = new System.Drawing.Size(75, 24);
             this.lblSearch.TabIndex = 4;
             this.lblSearch.Text = "Search:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(0, 12);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(255, 44);
+            this.label1.Size = new System.Drawing.Size(127, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Select Folder:";
             // 
@@ -237,9 +258,107 @@
             this.tsmOpenImage.Text = "Open";
             this.tsmOpenImage.Click += new System.EventHandler(this.tsmOpenFile_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            ">",
+            "<",
+            "="});
+            this.comboBox1.Location = new System.Drawing.Point(738, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(30, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = ">";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(774, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(82, 23);
+            this.textBox1.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            ">",
+            "<",
+            "="});
+            this.comboBox2.Location = new System.Drawing.Point(862, 13);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(30, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.Text = ">";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(635, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Size filter:";
+            // 
             // indexerControllerBindingSource
             // 
             this.indexerControllerBindingSource.DataSource = typeof(FileIndexer.Controller.IndexerController);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Creation date:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(136, 54);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(342, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 24);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Last modified date:";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(514, 54);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(734, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 24);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Extention:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox2.Location = new System.Drawing.Point(834, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 23);
+            this.textBox2.TabIndex = 15;
             // 
             // FileIndex
             // 
@@ -296,6 +415,16 @@
         private System.Windows.Forms.PictureBox pbFileImage;
         private System.Windows.Forms.ContextMenuStrip cmsNodeClick;
         private System.Windows.Forms.ToolStripMenuItem tsmOpenImage;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
